b8ecb04ba5a61db0e84020653ebc7694307c3bbb
LeePlumbe <leeplumbe@gmail.com>
2017-08-11 19:04:32 +1000

    Add files via upload
    Part of task 2.1

---
+++ sum1.py
@@ -0,0 +1,18 @@
+# a program to add two numbers
+#learning to get input, write comments, turn string values into integers
+
+# get the user to input the first number
+input1 = input('This program will add two numbers. Type in the first number, then press enter. ')
+
+# turn this input into a number
+a = int(input1)
+
+# get the user to input the second number
+input2 = input('Thank you. Type in the second number, then press enter. ')
+
+# turn this input into a number
+b = int(input2)
+
+# add the two numbers together, and output the anser
+sum = (a + b)
+print('The sum of these number is',sum)
---
+++ sum2.py
@@ -0,0 +1,7 @@
+#short program
+input1 = input('First number? ')
+a = int(input1)
+input2 = input('Second number? ')
+b = int(input2)
+y = (a + b)
+print('Sum =',y)
